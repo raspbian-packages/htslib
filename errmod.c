3e9e17ae2faf2a430db2f4a02f2788b8d7a6c4ae
Debian Med Packaging Team <debian-med-packaging@lists.alioth.debian.org>
2017-11-08 07:24:26 +0000

    877670
    Gbp-Pq: Name 877670.patch

--- errmod.c
+++ errmod.c
@@ -82,10 +82,11 @@ static void cal_coef(errmod_t *em, double depcorr, double eta)
         double le1 = log(1.0 - e);
         for (n = 1; n <= 255; ++n) {
             double *beta = em->beta + (q<<16|n<<8);
-            sum1 = sum = 0.0;
-            for (k = n; k >= 0; --k, sum1 = sum) {
-                sum = sum1 + expl(lC[n<<8|k] + k*le + (n-k)*le1);
-                beta[k] = -10. / M_LN10 * logl(sum1 / sum);
+            sum1 = lC[n<<8|n] + n*le;
+            beta[n] = INFINITY;
+            for (k = n - 1; k >= 0; --k, sum1 = sum) {
+                sum = sum1 + log1pl(expl(lC[n<<8|k] + k*le + (n-k)*le1 - sum1));
+                beta[k] = -10. / M_LN10 * (sum1 - sum);
             }
         }
     }
